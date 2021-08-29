e15293aa0bd61d59f1d03aca6f380238872e8932
Jerry-Heyihang <54059237+Jerry-Heyihang@users.noreply.github.com>
2021-08-29 10:47:50 +0800

    Update clock-in.py

--- clock-in.py
+++ clock-in.py
@@ -95,8 +95,8 @@ class DaKa(object):
         new_info['number'] = number
         new_info["date"] = self.get_date()
         new_info["created"] = round(time.time())
-        new_info["address"] = "广东省深圳市龙华区"
-        new_info["area"] = "广东省 深圳市 龙华区"
+        new_info["address"] = "浙江省嘉兴市海宁市"
+        new_info["area"] = "浙江省 嘉兴市 海宁市"
         new_info["province"] = new_info["area"].split(' ')[0]
         new_info["city"] = new_info["area"].split(' ')[1]
         # form change
